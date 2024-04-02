f848778a6786a3708cdb3610244fc18aedf8df46
grete <armei.grete@gmail.com>
2024-04-02 15:01:17 +0300

    nüüd õigesti

---
+++ oop_ryhmatoo1/src/FilmideKataloog.java
@@ -0,0 +1,17 @@
+/*public class FilmideKataloog {
+    private String filmiNimi;
+    private String sisestajaNimi;
+    private String žanr;
+    private double hinnang;
+
+    public FilmideKataloog(String filmiNimi, String sisestajaNimi, String žanr, double hinnang) {
+        this.filmiNimi = filmiNimi;
+        this.sisestajaNimi = sisestajaNimi;
+        this.žanr = žanr;
+        this.hinnang = hinnang;
+    }
+
+    public String lisaFilm(String filmiNimi, String sisestajaNimi, String žanr, double hinnang){
+
+    }
+}*/
